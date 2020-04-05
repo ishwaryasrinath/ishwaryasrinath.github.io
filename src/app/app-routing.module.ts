a8c9c1d8eb118f24ae7120e2c105d359e1d24d6c
ishwaryasrinath <63074050+ishwaryasrinath@users.noreply.github.com>
2020-04-05 12:37:19 +0530

    build updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyCartComponent } from './my-cart/my-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path : '',
+  component : MyCartComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MyCartComponent } from './my-cart/my-cart.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CheckoutComponent } from './checkout/checkout.component';
+import { MatDialogModule } from '@angular/material';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCartComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
+  entryComponents:[CheckoutComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
